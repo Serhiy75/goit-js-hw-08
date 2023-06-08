d49c05773cab9b43c70c7814ac7a39c359263dd9
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-08 22:37:13 +0300

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe#vimeo-player');
+const newPlayer = new Player(iframe);
+
+function playTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+newPlayer.on('timeupdate', throttle(playTime, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+    newPlayer.setCurrentTime(savedTime); 
+};
+
 
