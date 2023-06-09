666860e260ec15e73e22b1bfaa222edd7613d540
sstfnk3107 <s.stefanyk75@gmail.com>
2023-06-10 02:11:50 +0300

    third task done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+
+const form = document.querySelector('feedback-form');
+const storageKey = 'feedback-form-state';
+const throttledSaveStateToLocalStorage = throttle(saveStateToLocalStorage, 500);
+form.addEventListener('input', throttledSaveStateToLocalStorage);
+form.addEventListener('submit', handlSubmit);
+
+function saveStateToLocalStorage() {
+    const state = {};
+    for (const element of form.elements) {
+        if (element.tageName === 'INPUT' || element.tageName === 'TEXTAREA') {
+            state[element.name] = element.value;
+        };
+    };
+    localStorage.setItem(storageKey, JSON.stringify(state));
+
+};
+
+function clearStateFromLocalStorage() {
+    localStorage.removeItem(storageKey);
+};
+
+function handlSubmit(event) {
+    event.preventDefault();
+    const state = {};
+    for (const element of form.elements) {
+        if (element.tageName === 'INPUT' || element.tageName === 'TEXTAREA') {
+            state[element.name] = element.value;
+        };
+    };
+    clearStateFromLocalStorage();
+    form.reset();
+};
+
 
